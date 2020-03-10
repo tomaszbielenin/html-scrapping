bd9a3a2445a3b8163139bad512de9e6732e21758
Bielenin <Tomasz.Bielenin@jacobs.com>
2020-03-10 11:02:31 +0100

    cleanup

--- htmlscrapper.py
+++ htmlscrapper.py
@@ -1,72 +1,67 @@
-# Import libraries
-# Body
-# Save to excel
-
-# import easygui as eg
+# Add file naming
+# Add batch mode
+# Add some UI
 
 from bs4 import BeautifulSoup as BS
+import pandas as pd
 import re
 
-with open('C:/Scripting/Git/html-scrapping/d0178777.HTM') as fp:
-    results = BS(fp).find(id="ResultsTable")
+def dwgname(dwg):
+    name = dwg.find_all('table')[1].find('font').text.split('\\')[-1].rstrip()
+    return name
 
-dwgs = results.find_all('font', {'class':'labelText4Blue'})
-dwgnames = []
-for dwg in dwgs:
-    dwgnames.append(dwg.text.split('\\')[-1].rstrip())
+def dwgproblems(dwg):
+    problems = dwg.find_all('table')[3].find('tr').find_next_siblings('tr')
+    return problems
 
-# from pandas import DataFrame
-import pandas as pd
-# l1 = [1,2,3,4]
-# l2 = [1,2,3,4]
-# df = pd.DataFrame({'Stimulus Time': l1, 'Reaction Time': l2})
-# dwgs = []
-problems = list(range(0,len(dwgnames)))
-names = list(range(0,len(dwgnames)))
-properties = list(range(0,len(dwgnames)))
-values = list(range(0,len(dwgnames)))
+with open('C:/Scripting/Git/html-scrapping/d0178772.HTM') as fp:
+    # results = BS(fp).find(id="ResultsTable")
+    results = BS(fp).find_all('table',id=re.compile('ErrDwg[0-9]*'))
 
-df = pd.DataFrame({'Files': dwgnames, 'Type': problems, 'Data': names, 'Property': properties, 'Current Value': values})
-df
+i=0
+dct = {}
+for dwg in results:
+    dproblems = dwgproblems(dwg)
+    dname = dwgname(dwg)
+    for tr in dproblems:
+        if tr.find_all('table'):
+            itemname = tr.find('td').font.text
+            for itr in tr.find('table').find('tr').find_next_siblings('tr'): 
+                if len(itr.find_all('td'))>1:
+                    dct['lst_%s' % (str(i))] = []
+                    dct['lst_%s' % (str(i))].append(dname)
+                    dct['lst_%s' % (str(i))].append(itemname)
+                    for td in itr.find_all('td'):
+                        dct['lst_%s' % (str(i))].append(td.font.text.rstrip().lstrip())
+                    i+=1 
+        elif len(tr.find_all('td'))==1:
+            dct['lst_%s' % (str(i))] = []
+            dct['lst_%s' % (str(i))].append(dname)
+            dct['lst_%s' % (str(i))].append(tr.td.font.text.rstrip().lstrip())
+            i+=1    
+        else:
+            dct['lst_%s' % (str(i))] = []
+            dct['lst_%s' % (str(i))].append(dname)
+            for td in tr.find_all('td'):
+                dct['lst_%s' % (str(i))].append(td.font.text.rstrip().lstrip())
+            i+=1
 
-df.to_excel('Filename.xlsx', sheet_name='SheetName', index=False)
+for key in dct:
+    while len(dct[key])<5:
+        dct[key].append('')
 
-# Save to csv
-# f = open('example.csv','w')
-# f.write("display,variable x")
-# f.close()
-
-# Library to work with excel
-# import xlwt
-
-# def output(filename, sheet, list1, list2, x, y, z):
-#     book = xlwt.Workbook()
-#     sh = book.add_sheet(sheet)
-
-#     variables = [x, y, z]
-#     x_desc = 'Display'
-#     y_desc = 'Dominance'
-#     z_desc = 'Test'
-#     desc = [x_desc, y_desc, z_desc]
-
-#     col1_name = 'Stimulus Time'
-#     col2_name = 'Reaction Time'
-
-#     #You may need to group the variables together
-#     #for n, (v_desc, v) in enumerate(zip(desc, variables)):
-#     for n, v_desc, v in enumerate(zip(desc, variables)):
-#         sh.write(n, 0, v_desc)
-#         sh.write(n, 1, v)
-
-#     n+=1
-
-#     sh.write(n, 0, col1_name)
-#     sh.write(n, 1, col2_name)
-
-#     for m, e1 in enumerate(list1, n+1):
-#         sh.write(m, 0, e1)
-
-#     for m, e2 in enumerate(list2, n+1):
-#         sh.write(m, 1, e2)
-
-#     book.save(filename)
+drawings = []
+items = []
+properties = []
+current = []
+standard = []
+    
+for key in dct:
+    drawings.append(dct[key][0])
+    items.append(dct[key][1])
+    properties.append(dct[key][2])
+    current.append(dct[key][3])
+    standard.append(dct[key][4])
+        
+df = pd.DataFrame({'Drawing' : drawings, 'Item': items, 'Property': properties, 'Current' : current, 'Standard' : standard})
+df.to_excel('d0178772.xlsx', sheet_name='Problems', index=False)
